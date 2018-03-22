9b52e2fd047b781dab421cad4107ed36cb5a608d
Matias Pierobon <matias.pierobonm@gmail.com>
2018-03-22 10:44:51 -0300

    delete pruebas dir

--- pruebas/src/features/profile/pages/profile/pages/profile-settings/profile-settings.ts
+++
@@ -1,8 +0,0 @@
-import { IonicPage } from "ionic-angular"
-import { Component } from "@angular/core"
-
-@IonicPage()
-@Component({
-  templateUrl: "profile-settings.html"
-})
-export class ProfileSettingsPage {}
